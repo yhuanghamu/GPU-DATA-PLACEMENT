f4fece33d3b293692a258c2638498f012c8d1934
Yingchao Huang <stevenhuang4321@gmail.com>
2016-02-24 17:06:08 -0800

    update 1.cu

--- spmv_prof/1.cu
+++ spmv_prof/1.cu
@@ -165,8 +165,10 @@ spmv_kernel(const float* val,
   __shared__ float s_vec[spmv_NBLOCKS * (spmv_BLOCK_SIZE/WARP_SIZE)];
   
   int tid = blockIdx.x*spmv_BLOCK_SIZE+threadIdx.x;
-  if (tid < spmv_NBLOCKS * (spmv_BLOCK_SIZE/WARP_SIZE))
-			s_vec[tid]= vec[tid];
+ // if (tid < spmv_NBLOCKS * (spmv_BLOCK_SIZE/WARP_SIZE))
+  if (tid == 0) {
+	  for (int i = 0 ; i<(spmv_NBLOCKS * (spmv_BLOCK_SIZE/WARP_SIZE));i++)
+			s_vec[i]= vec[i];
 //  if(t==spmv_BLOCK_SIZE-1) rowDeli[t/WARP_SIZE+1]= rowDelimiters[myRow+1];
   __syncthreads();
   
