e81fffafed06e9bb40635547f8e97ff5f106232e
Yingchao Huang <stevenhuang4321@gmail.com>
2016-03-09 14:24:21 -0800

    update MD

--- MD/compile.sh
+++ MD/compile.sh
@@ -1,3 +1,14 @@
-nvcc md_index.cu -o md_index -arch=sm_20
-nvcc md.cu -o md -arch=sm_20
-nvcc 1.cu -o 1 -arch=sm_20
+FLAGS ?= -arch=sm_30
+all:
+	nvcc md_index.cu -o $(FLAGS) 
+	nvcc md.cu -o md $(FLAGS)
+	nvcc 1.cu -o 1 $(FLAGS)
+	nvcc 2.cu -o 2 $(FLAGS)
+	nvcc 3.cu -o 3 $(FLAGS)
+	nvcc 4.cu -o 4 $(FLAGS)
+	nvcc 4_overhead.cu -o 4_overhead $(FLAGS)
+	nvcc 5.cu -o 5 $(FLAGS)
+	nvcc 6.cu -o 6 $(FLAGS)
+	nvcc 6_1_overhead.cu -o 6_1_overhead $(FLAGS)
+	nvcc 6_overhead.cu -o 6_overhead $(FLAGS)
+	nvcc 7.cu -o 7 $(FLAGS)
